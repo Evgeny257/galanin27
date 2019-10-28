38de2d958c9331816c347c068d3051f8de1324ca
Evgeny257 <galanin.zhenia@yandex.ru>
2019-10-28 18:20:32 +0300

    Create galanin.cpp
    First func

---
+++ galanin.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+void privet()
+{
+	printf("Привет");
+}
+void poka()
+{
+	printf("Пока");
+}
+int galanin(int argc, char** argv)
+{
+	privet();
+	poka();
+	return 0;
+}
